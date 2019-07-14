a50f2cf92ce51f591bd0b6b5ea2da51e9f4895d5
StragaSevera <ought@yandex.ru>
2019-07-15 01:48:15 +0300

    Add bootstrap styling

--- SportsStore/Helpers/PageLinkTagHelper.cs
+++ SportsStore/Helpers/PageLinkTagHelper.cs
@@ -12,10 +12,14 @@ namespace SportsStore.Helpers
     {
         [ViewContext]
         [HtmlAttributeNotBound]
+        // ReSharper disable once MemberCanBePrivate.Global
         public ViewContext ViewContext { get; set; }
 
         public PagingInfo PageModel { get; set; }
         public string PageAction { get; set; }
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
 
         private readonly IUrlHelperFactory _urlHelperFactory;
 
@@ -32,6 +36,11 @@ namespace SportsStore.Helpers
             {
                 var tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction, new {page = i});
+                if (!string.IsNullOrWhiteSpace(PageClass))
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
                 if (i < PageModel.TotalPages) result.InnerHtml.Append(" ");
